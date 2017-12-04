5c37d235a22e3572fc18f039f38c4ddfdab8d379
Jeroen Geusebroek <me@jeroengeusebroek.nl>
2017-12-04 18:07:33 +0100

    New Molecule test template

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,11 +1,13 @@
+import os
+
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(File):
-    f = File('/etc/hosts')
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
 
     assert f.exists
     assert f.user == 'root'
